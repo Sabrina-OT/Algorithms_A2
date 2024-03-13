3346c5e694de39a30b8b22d8d8ac8c3e1360bce8
Sabrina-OT <sabrina.skoric@ontariotechu.net>
2024-03-13 13:19:59 -0400

    first commit

---
+++ Assignment2.py
@@ -0,0 +1,12 @@
+#Sabrina Skoric: 100872613
+#Algorithms and Data Structures Assignment 2
+
+def main():
+    ID_ARRAY = [11, 1, 30, 2, 51, 6, 29, 7, 67, 15, 118, 4, 89, 23]
+
+    print("Here is the current array:", ID_ARRAY)
+
+
+        
+if __name__ == "__main__":
+    main()
